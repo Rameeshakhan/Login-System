14b412f51ac19198c3cab87b6ff11bd2cf5a5f3f
Rameesha Khan <rameeshakhan75@gmail.com>
2023-02-12 00:46:01 +0500

    ser Added to database

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const app = express();
+const cors = require("cors")
+const mongoose = require("mongoose")
+const User = require("./model/user.model")
+
+require("dotenv").config();
+const URL = process.env.DATABASE;
+app.use(cors())
+app.use(express.json())
+
+//connecting databse using mongoose
+mongoose.connect(URL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(error => {
+        console.log('Error connecting to MongoDB:', error);
+    });
+
+//create user route
+app.post('/api/register', async (req, res) => {
+	console.log(req.body)
+	try {
+		// const newPassword = await bcrypt.hash(req.body.password, 10)
+		await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+		res.json({ status: 'error', error: 'Duplicate email' })
+	}
+})
+
+//login route
+app.post("/api/login", async (req, res) => {
+    await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    })
+    if(user){
+        res.json({ status: "ok" , user: true})
+    } else{
+        res.json({ status: "error", error: "User not found" })
+    }
+
+})
+
+app.listen(2000, () => {
+    console.log("Server started at 2000")
+})
