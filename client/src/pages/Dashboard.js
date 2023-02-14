8d09b815b5778a8947768bf4f1ac3b4a0736e42c
Rameesha Khan <rameeshakhan75@gmail.com>
2023-02-14 21:05:23 +0500

    Dashboard MOdified

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -8,6 +8,9 @@ const Dashboard = () => {
           <img src={dashboard} alt="BigCo Inc. logo"/>
     </div>
     <button className='btn dash'>About US</button>
+    <div>
+      <p>This is paragraph added by Me in this project</p>
+    </div>
    </>
   )
 }
